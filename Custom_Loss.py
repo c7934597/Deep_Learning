279c7db4fb7af6564f72b0da8e75bb0d8544afa6
Wei-Ming Lin <c7934597@gmail.com>
2021-05-04 01:42:24 +0800

    update

--- Custom_Loss.py
+++ Custom_Loss.py
@@ -1,24 +1,65 @@
 # pytorch深度學習模組套件
 import torch
-import torch.nn as nn
+from torch.nn.modules.loss import _WeightedLoss
 import torch.nn.functional as F
 
-class build_custiom_model(nn.Module):
+'''
+https://www.kaggle.com/c/siim-isic-melanoma-classification/discussion/173733
+'''
+class SmoothCrossEntropyLoss(_WeightedLoss):
+    def __init__(self, weight=None, reduction='mean', smoothing=0.0):
+        super().__init__(weight=weight, reduction=reduction)
+        self.smoothing = smoothing
+        self.weight = weight
+        self.reduction = reduction
 
-    def __init__(self):
-        super().__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+    @staticmethod
+    def _smooth_one_hot(targets:torch.Tensor, n_classes:int, smoothing=0.0):
+        assert 0 <= smoothing < 1
+        with torch.no_grad():
+            targets = torch.empty(size=(targets.size(0), n_classes),
+                    device=targets.device) \
+                .fill_(smoothing /(n_classes-1)) \
+                .scatter_(1, targets.data.unsqueeze(1), 1.-smoothing)
+        return targets
 
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+    def forward(self, inputs, targets):
+        targets = SmoothCrossEntropyLoss._smooth_one_hot(targets, inputs.size(-1),
+            self.smoothing)
+        lsm = F.log_softmax(inputs, -1)
+
+        if self.weight is not None:
+            lsm = lsm * self.weight.unsqueeze(0)
+
+        loss = -(targets * lsm).sum(-1)
+
+        if  self.reduction == 'sum':
+            loss = loss.sum()
+        elif  self.reduction == 'mean':
+            loss = loss.mean()
+
+        return loss
+
+'''
+https://www.kaggle.com/c/tgs-salt-identification-challenge/discussion/65938
+'''
+class FocalLoss(nn.Module):
+    def __init__(self, alpha=1, gamma=2, logits=False, reduce=True):
+        super(FocalLoss, self).__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+        self.logits = logits
+        self.reduce = reduce
+
+    def forward(self, inputs, targets):
+        if self.logits:
+            BCE_loss = F.binary_cross_entropy_with_logits(inputs, targets, reduce=False)
+        else:
+            BCE_loss = F.binary_cross_entropy(inputs, targets, reduce=False)
+        pt = torch.exp(-BCE_loss)
+        F_loss = self.alpha * (1-pt)**self.gamma * BCE_loss
+
+        if self.reduce:
+            return torch.mean(F_loss)
+        else:
+            return F_loss

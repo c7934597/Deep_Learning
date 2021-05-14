75ebc0a1933d5c050ed55b2a33adb8e09ece13b4
Wei-Ming Lin <c7934597@gmail.com>
2021-05-14 14:51:27 +0800

    update

--- Custom_Loss.py
+++ Custom_Loss.py
@@ -63,3 +63,82 @@ class FocalLoss(nn.Module):
             return torch.mean(F_loss)
         else:
             return F_loss
+
+'''
+https://www.kaggle.com/bigironsphere/loss-function-library-keras-pytorch
+'''
+class DiceLoss(nn.Module):
+    def __init__(self, weight=None, size_average=True, smoothing=1):
+        super(DiceLoss, self).__init__()
+        self.weight = weight
+        self.size_average = size_average
+        self.smoothing = smoothing
+
+    def forward(self, inputs, targets):
+        
+        #comment out if your model contains a sigmoid or equivalent activation layer
+        inputs = F.sigmoid(inputs)       
+        
+        #flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        
+        intersection = (inputs * targets).sum()                            
+        dice = (2.*intersection + self.smoothing)/(inputs.sum() + targets.sum() + self.smoothing)  
+        
+        return 1 - dice
+
+'''
+https://www.kaggle.com/bigironsphere/loss-function-library-keras-pytorch
+'''
+class DiceBCELoss(nn.Module):
+    def __init__(self, weight=None, size_average=True,  smoothing=1):
+        super(DiceBCELoss, self).__init__()
+        self.weight = weight
+        self.size_average = size_average
+        self.smoothing = smoothing
+
+    def forward(self, inputs, targets):
+        
+        #comment out if your model contains a sigmoid or equivalent activation layer
+        inputs = F.sigmoid(inputs)       
+        
+        #flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        
+        intersection = (inputs * targets).sum()                            
+        dice_loss = 1 - (2.*intersection +  self.smoothing)/(inputs.sum() + targets.sum() +  self.smoothing)  
+        BCE = F.binary_cross_entropy(inputs, targets, reduction='mean')
+        Dice_BCE = BCE + dice_loss
+        
+        return Dice_BCE
+
+'''
+https://www.kaggle.com/bigironsphere/loss-function-library-keras-pytorch
+'''
+class IoULoss(nn.Module):
+    def __init__(self, weight=None, size_average=True,  smoothing=1):
+        super(IoULoss, self).__init__()
+        self.weight = weight
+        self.size_average = size_average
+        self.smoothing = smoothing
+
+    def forward(self, inputs, targets):
+        
+        #comment out if your model contains a sigmoid or equivalent activation layer
+        inputs = F.sigmoid(inputs)       
+        
+        #flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        
+        #intersection is equivalent to True Positive count
+        #union is the mutually inclusive area of all labels & predictions 
+        intersection = (inputs * targets).sum()
+        total = (inputs + targets).sum()
+        union = total - intersection 
+        
+        IoU = (intersection + self.smoothing)/(union + self.smoothing)
+                
+        return 1 - IoU
